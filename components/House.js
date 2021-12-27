6aaa3da795ce215552433a889ae05e62acd69327
Dmytro Ben <dmitriy.ben@gmail.com>
2021-12-27 17:31:35 +0200

    feat(js): init

---
+++ components/House.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default function House(props) {
+  console.log(props);
+
+  return (
+    <Link href="/houses/[id]" as={'/houses/' + props.id}>
+      <a>
+        <img src={props.picture} width="100%" alt="House picture" />
+        <p>
+          {props.type} - {props.town}
+        </p>
+        <p>{props.title}</p>
+      </a>
+    </Link>
+  )
+}
---
+++ pages/houses/[id].js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import houses from '../../houses.js'
+import Layout from '../../components/Layout'
+import DateRangePicker from '../../components/DateRangePicker'
+
+export default function House(props) {
+  return (
+    <Layout
+      content={
+        <div className="container">
+          <Head>
+            <title>{props.house.title}</title>
+          </Head>
+          <article>
+            <img src={props.house.picture} width="100%" alt="House picture" />
+            <p>
+              {props.house.type} - {props.house.town}
+            </p>
+            <p>{props.house.title}</p>
+          </article>
+          <aside>
+          <h2>Choose a date</h2>
+            <DateRangePicker />
+          </aside>
+
+          <style jsx>
+            {`
+            .container {
+              display: grid;
+              grid-template-columns: 60% 40%;
+              grid-gap: 30px;
+            }
+
+            aside {
+              border: 1px solid #ccc;
+              padding: 20px;
+            }
+          `}
+          </style>
+        </div>
+      }
+    />
+  )
+}
+
+export async function getServerSideProps({ query }) {
+  const { id } = query
+
+  return {
+    props: {
+      house: houses.filter((house) => house.id === parseInt(id))[0]
+    }
+  }
+}
