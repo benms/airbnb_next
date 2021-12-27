7b99c4d48e7fe08ec9fa2fd413cd76da90c8db4d
Dmytro Ben <dmitriy.ben@gmail.com>
2021-12-27 19:30:31 +0200

    feat(js): added modal window

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,17 @@
 import Header from './Header'
+import Modal from './Modal'
+import { useState } from 'react'
 
 export default function Layout(props) {
+  const [showModal, setShowModal] = useState(true)
+
   return (
     <div>
       <Header />
       <main>{props.content}</main>
+      {
+        showModal && <Modal close={() => setShowModal(false)}>test</Modal>
+      }
       <style jsx>{`
         main {
           position: relative;
