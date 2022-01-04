1810a8949ebe9c0146bdafe5c8e8c4f8d2eb5cd9
Dmytro Ben <dmitriy.ben@gmail.com>
2022-01-04 14:49:59 +0200

    feat(js): User model with salted password

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,10 +1,28 @@
-const registerRequest = (req, res) => {
+import { User } from '../../../model.js'
+
+const registerRequest = async (req, res) => {
   if (req.method !== 'POST') {
     res.status(405).end() //Method Not Allowed
     return
   }
-  console.log('POST request received', req.body)
-  res.end()
+
+  try {
+    const { email, password, passwordConfirmation } = req.body
+
+    if (password !== passwordConfirmation) {
+      res.end(
+        JSON.stringify({ status: 'error', message: 'Passwords do not match' })
+      )
+      return
+    }
+
+    const user = await User.create({ email, password })
+    res.end(JSON.stringify({ status: 'success', message: 'User added', user }))
+  }
+  catch (err) {
+    console.error(err)
+  }
+
 }
 
 export default registerRequest
